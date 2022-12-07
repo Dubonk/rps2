02f4d13dd6cca32e25031a28bfa87f4408a3b0c7
kevin <kevindubon43@gmail.com>
2022-12-06 16:08:32 -0800

    Game now ends on 5th win. Still needs to be styled.

--- script.js
+++ script.js
@@ -13,22 +13,66 @@ const scissors = document.querySelector('#scissors');
         playRound('Scissors');
     })
 
+    const reset = document.querySelector('#reset');
+reset.addEventListener('click', () => {
+    resetGame();
+});    
 
-
-
-//computer was getting a random value but because it was declared in the global scope, the value stayed the same in the function game()//
+const score = document.querySelector('#score');
+score.textContent = `You: 0 || COM: 0 || Tie: 0`;
+const results = document.querySelector('#results');
+const final = document.querySelector('#final');
 
 let pc = '';
-let win;
-let loss; 
-let tie = `It's a tie!`;
-let wrong;
 let userScore = 0;
 let comScore = 0;
 let tieScore = 0;
 
+//This function will compare the player's choice and the computer's choice, then score results.//
+function playRound(userChoice) {
+    pc = getComputerChoice();
+    if(userScore < 5 && comScore < 5){
+    if (pc === "Rock" && userChoice === "Paper" || pc === "Paper" && userChoice === "Scissors" || pc === "Scissors" && userChoice === "Rock") {
+        userScore++;
+        score.textContent = `You: ${userScore} || COM: ${comScore} || Tie: ${tieScore}`;
+        results.textContent = `Win: ${userChoice} beats ${pc}!`;
+        endGame();
+    }
 
-//This function will randomly return 'rock paper or scissors'//
+    else if (userChoice === "Paper" && pc === "Scissors" || userChoice === "Scissors" && pc === "Rock" || userChoice === "Rock" && pc === "Paper") {
+        comScore++;
+        score.textContent = `You: ${userScore} || COM: ${comScore} || Tie: ${tieScore}`;
+        results.textContent = `Lose: ${pc} beats ${userChoice}...`;
+        endGame();
+    }
+
+    else if ( userChoice == pc) {
+        tieScore++;
+        score.textContent = `You: ${userScore} || COM: ${comScore} || Tie: ${tieScore}`;
+        results.textContent = `It's a tie!`;
+        }}
+}
+
+//This function will compare scores// 
+function endGame() {
+    if (userScore > 4 && comScore < 5) {
+        final.textContent = "Game Over!" + "\n" + `You win ${userScore} to ${comScore}`;
+    }
+    else if (comScore > 4 && userScore < 5) {
+        final.textContent = "Game Over..." + '\n' +`You lose ${comScore} to ${userScore}`;
+    }
+}
+
+//Helper Functions//
+function resetGame() {
+    score.textContent = `You: 0 || COM: 0 || Tie: 0`;
+    final.textContent = '';
+    results.textContent = '';
+    comScore = 0;
+    userScore = 0;
+    tieScore = 0;
+}
+
 function getComputerChoice() {
     let ranNum = Math.floor(Math.random() * 3) + 1;
 
@@ -42,62 +86,3 @@ function getComputerChoice() {
         return "Scissors";
     }
 }
-
-//This function will compare the player's choice and the computer's choice, then return win, loss. tie or error for spelling.//
-function playRound(userChoice) {
-    pc = getComputerChoice();
-    if(userScore < 5 && comScore < 5){
-    if (pc === "Rock" && userChoice === "Paper" || pc === "Paper" && userChoice === "Scissors" || pc === "Scissors" && userChoice === "Rock") {
-        ++userScore;
-        score.textContent = `You: ${userScore} || COM: ${comScore} || Tie: ${tieScore}`;
-        results.textContent = `${userChoice} beats ${pc}!`;
-    }
-
-    else if (userChoice === "Paper" && pc === "Scissors" || userChoice === "Scissors" && pc === "Rock" || userChoice === "Rock" && pc === "Paper") {
-        ++comScore;
-        score.textContent = `You: ${userScore} || COM: ${comScore} || Tie: ${tieScore}`;
-        results.textContent = `${pc} beats ${userChoice}...`;
-    }
-
-    else if ( userChoice == pc) {
-        ++tieScore;
-        score.textContent = `You: ${userScore} || COM: ${comScore} || Tie: ${tieScore}`;
-        results.textContent = `It's a tie!`;
-        
-    }}
-    else {
-        endGame();
-    }
-    
-}
-
-//This function will compare scores and console log it.// 
-function endGame() {
-    if (userScore > comScore) {
-        final.textContent = "Game Over!" + "\n" + `You win ${userScore} to ${comScore}`;
-    }
-    else if (comScore > userScore) {
-        final.textContent = "Game Over..." + '\n' +`You lose ${comScore} to ${userScore}`;
-    }
-}
-
-function resetGame() {
-    score.textContent = `You: 0 || COM: 0 || Tie: 0`;
-    final.textContent = '';
-    results.textContent = '';
-    comScore = 0;
-    userScore = 0;
-    tieScore = 0;
-}
-
-const reset = document.querySelector('#reset');
-reset.addEventListener('click', () => {
-    resetGame();
-});    
-
-const score = document.querySelector('#score');
-score.textContent = `You: 0 || COM: 0 || Tie: 0`;
-const results = document.querySelector('#results');
-const final = document.querySelector('#final');
-
-
